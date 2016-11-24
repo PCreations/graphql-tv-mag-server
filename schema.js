4f467137964a31dee0d534b0d3cd0797436f00d6
Pierre Criulanscy <pcriulan@gmail.com>
2016-11-24 17:25:47 +0100

    first version of simple schema is ok

--- schema.js
+++ schema.js
@@ -15,6 +15,18 @@ import {
 
 const API_TVMAG_ROOT = 'http://api-tvmag.lefigaro.fr/'
 const PRIME_TIME_ENDPOINT = 'http://api-tvmag.lefigaro.fr/1.0/schedules/prime-time?is_default=1'
+const HEADLINES_ENDPOINT = 'http://api.fidji.lefigaro.fr/export/articles/?source=tvmag.lefigaro.fr&type=ART&type=LIV&limit=7&full=0&oneprofile=1&mediaref=1&section_id=36001'
+const SERIES_HEADLINES_ENDPOINT = 'http://api.fidji.lefigaro.fr/export/articles/?source=tvmag.lefigaro.fr&type=ART&section=S%C3%A9ries&limit=3&full=0&oneprofile=1&mediaref=1'
+const PEOPLE_HEADLINES_ENDPOINT = 'http://api.fidji.lefigaro.fr/export/articles/?source=tvmag.lefigaro.fr&limit=6&full=0&oneprofile=1&mediaref=1&section_id=36006'
+
+const HeadlinesType = new GraphQLEnumType({
+  name: 'HeadlinesType',
+  values: {
+    INFO: { value: HEADLINES_ENDPOINT },
+    SERIES: { value: SERIES_HEADLINES_ENDPOINT },
+    PEOPLE: { value: PEOPLE_HEADLINES_ENDPOINT },
+  }
+})
 
 const ArticleExcerpt = new GraphQLObjectType({
   name: 'Article',
@@ -52,6 +64,23 @@ const Query = new GraphQLObjectType({
             thumbnail: `${API_TVMAG_ROOT}${program.photos[0]}`,
             title: program.title,
             startAt: broadcast.start_at
+          }
+        }))
+      )
+    },
+    headlines: {
+      type: new GraphQLList(ArticleExcerpt),
+      args: {
+        type: { type: HeadlinesType }
+      },
+      resolve: (root, { type }) => (
+        fetch(type)
+        .then(res => res.json())
+        .then(jsonRes => jsonRes.news.feed.map(feedEntry => {
+          return {
+            thumbnail: feedEntry.default.image,
+            title: feedEntry.default.title,
+            description: feedEntry.default.snippet
           }
         }))
       )
